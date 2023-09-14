ef69d55ebb104a7c9164f03e05474529e610db85
Chethanshetty4 <118925283+Chethanshetty4@users.noreply.github.com>
2023-09-14 15:34:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+'use script';
+
+var featured = document.getElementById("featured");
+console.log(featured);
+
+var btn = document.querySelector(".btndiv1 button")
+console.log(btn);
+
+var img1 = document.querySelector('#img1');
+
+var img2 = document.querySelectorAll("#img2");
+
+var fleftElement = document.querySelectorAll(".fleftElement");
+
+var images = document.querySelector(".images");
+
+btn.addEventListener('click', ()=>{
+    featured.style.marginBottom = '140vw';
+    img1.style.marginBottom = "5vw";
+    img2.forEach(element => {
+        element.style.display = 'block';
+        element.style.marginBottom = "5vw";
+    });
+    btn.style.display = "none";
+
+    fleftElement.forEach(element => {
+        element.style.display = "flex";
+    });
+})
+
+
+
+// display: flex;
